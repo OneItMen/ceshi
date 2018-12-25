c09ab48eb53c66e17c09d7737661edde1267601c
lyg <2100761313@qq.com>
2018-12-25 15:13:05 +0800

    hello2

--- src/main/java/com/guoyun/demo3/controller/HelloController.java
+++ src/main/java/com/guoyun/demo3/controller/HelloController.java
@@ -10,4 +10,10 @@ public class HelloController {
     public String hello(){
         return "hello";
     }
+
+
+    @RequestMapping("/hello2")
+    public String hello2(){
+        return "hello2";
+    }
 }
