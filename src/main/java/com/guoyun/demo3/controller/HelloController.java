cce973a980e1022745754b46d29337bfd03257b5
lyg <2100761313@qq.com>
2018-12-25 14:51:16 +0800

    测试环境

---
+++ src/main/java/com/guoyun/demo3/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.guoyun.demo3.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("hello")
+    public String hello(){
+        return "hello";
+    }
+}
